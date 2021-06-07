53b76ddd044927fb2a1fb72a590b56465450aadc
andrew <neon.duncan@gmail.com>
2021-06-07 12:45:05 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 62
+let num2 = 1
+
+const num3 = 24
+const num4 = 5
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1
+
+
+const mod = num3%num4
+
+const max = Math.max(20, 1, 3)
